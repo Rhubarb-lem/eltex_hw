ccc0ffd209ba7d82b211865221a8070e1bb13501
Rhubarb-lem <revin2003@yandex.ru>
2024-06-13 18:47:58 +0700

    Add base for chat implimitation

---
+++ hw13/task2/client.c
@@ -0,0 +1,189 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <mqueue.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <ncurses.h>
+
+#define QUEUE_NAME "/chat_queue"
+#define MAX_SIZE 1024
+#define MSG_STOP "exit"
+#define USER_JOINED "User joined: "
+#define USER_LEFT "User left: "
+
+mqd_t mq;
+char username[MAX_SIZE];
+pthread_mutex_t user_list_mutex = PTHREAD_MUTEX_INITIALIZER;
+char user_list[MAX_SIZE][MAX_SIZE];
+int user_count = 0;
+
+WINDOW *msg_win, *input_win, *user_win;
+
+void init_ncurses() {
+    initscr();
+    cbreak();
+    noecho();
+    curs_set(TRUE);  // Allow cursor to be visible for input
+    refresh();
+    int height, width;
+    getmaxyx(stdscr, height, width);
+
+    user_win = newwin(height, width / 4, 0, width - width / 4);
+    msg_win = newwin(height - 3, width - width / 4, 0, 0);
+    input_win = newwin(3, width - width / 4, height - 3, 0);
+
+    scrollok(msg_win, TRUE);
+    box(user_win, 0, 0);
+    box(input_win, 0, 0);
+}
+
+void close_ncurses() {
+    delwin(msg_win);
+    delwin(input_win);
+    delwin(user_win);
+    endwin();
+}
+
+void update_user_list() {
+    werase(user_win);
+    box(user_win, 0, 0);
+    mvwprintw(user_win, 1, 1, "Users:");
+    pthread_mutex_lock(&user_list_mutex);
+    for (int i = 0; i < user_count; ++i) {
+        mvwprintw(user_win, i + 2, 1, user_list[i]);
+    }
+    pthread_mutex_unlock(&user_list_mutex);
+    wrefresh(user_win);
+}
+
+void display_message(const char *message) {
+    wprintw(msg_win, "%s\n", message);
+    wrefresh(msg_win);
+}
+
+void *receive_messages(void *arg) {
+    char buffer[MAX_SIZE];
+    mqd_t receive_mq = mq_open(QUEUE_NAME, O_RDONLY);
+    if (receive_mq == -1) {
+        perror("Client: mq_open for receive");
+        exit(1);
+    }
+
+    while (1) {
+        ssize_t bytes_read = mq_receive(receive_mq, buffer, MAX_SIZE, NULL);
+        if (bytes_read < 0) {
+            perror("Client: mq_receive");
+            exit(1);
+        }
+        buffer[bytes_read] = '\0';
+
+        if (strncmp(buffer, MSG_STOP, strlen(MSG_STOP)) == 0) {
+            break;
+        }
+
+        if (strncmp(buffer, USER_JOINED, strlen(USER_JOINED)) == 0) {
+            pthread_mutex_lock(&user_list_mutex);
+            strncpy(user_list[user_count], buffer + strlen(USER_JOINED), MAX_SIZE - 1);
+            user_list[user_count][MAX_SIZE - 1] = '\0';
+            user_count++;
+            pthread_mutex_unlock(&user_list_mutex);
+            update_user_list();
+            display_message(buffer);
+        } else if (strncmp(buffer, USER_LEFT, strlen(USER_LEFT)) == 0) {
+            pthread_mutex_lock(&user_list_mutex);
+            for (int i = 0; i < user_count; ++i) {
+                if (strcmp(user_list[i], buffer + strlen(USER_LEFT)) == 0) {
+                    for (int j = i; j < user_count - 1; ++j) {
+                        strncpy(user_list[j], user_list[j + 1], MAX_SIZE);
+                    }
+                    user_count--;
+                    break;
+                }
+            }
+            pthread_mutex_unlock(&user_list_mutex);
+            update_user_list();
+            display_message(buffer);
+        } else {
+            display_message(buffer);
+        }
+    }
+
+    mq_close(receive_mq);
+    return NULL;
+}
+
+void *send_messages(void *arg) {
+    char buffer[MAX_SIZE];
+    char message[MAX_SIZE + 50]; // Reserve space for username
+    mqd_t send_mq = mq_open(QUEUE_NAME, O_WRONLY);
+    if (send_mq == -1) {
+        perror("Client: mq_open for send");
+        exit(1);
+    }
+
+    while (1) {
+        werase(input_win);
+        box(input_win, 0, 0);
+        mvwgetnstr(input_win, 1, 1, buffer, MAX_SIZE - 2);
+        wrefresh(input_win);
+
+        snprintf(message, sizeof(message), "%s: %s", username, buffer);
+        if (mq_send(send_mq, message, strlen(message) + 1, 0) == -1) {
+            perror("Client: mq_send");
+            exit(1);
+        }
+
+        if (strncmp(buffer, MSG_STOP, strlen(MSG_STOP)) == 0) {
+            break;
+        }
+    }
+
+    mq_close(send_mq);
+    return NULL;
+}
+
+int main() {
+    printf("Enter your username: ");
+    fgets(username, MAX_SIZE, stdin);
+    username[strcspn(username, "\n")] = '\0'; // Remove newline character
+
+    init_ncurses();
+
+    mq = mq_open(QUEUE_NAME, O_WRONLY);
+    if (mq == -1) {
+        perror("Client: mq_open");
+        close_ncurses();
+        exit(1);
+    }
+
+    char join_message[MAX_SIZE];
+    snprintf(join_message, sizeof(join_message), "%s%s", USER_JOINED, username);
+    if (mq_send(mq, join_message, strlen(join_message) + 1, 0) == -1) {
+        perror("Client: mq_send join_message");
+        close_ncurses();
+        exit(1);
+    }
+
+    pthread_t receive_thread, send_thread;
+    pthread_create(&receive_thread, NULL, receive_messages, NULL);
+    pthread_create(&send_thread, NULL, send_messages, NULL);
+
+    pthread_join(send_thread, NULL);
+
+    char leave_message[MAX_SIZE];
+    snprintf(leave_message, sizeof(leave_message), "%s%s", USER_LEFT, username);
+    if (mq_send(mq, leave_message, strlen(leave_message) + 1, 0) == -1) {
+        perror("Client: mq_send leave_message");
+    }
+
+    pthread_cancel(receive_thread);
+    pthread_join(receive_thread, NULL);
+
+    mq_close(mq);
+    close_ncurses();
+
+    return 0;
+}
---
+++ hw13/task2/server.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <mqueue.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#define QUEUE_NAME "/chat_queue"
+#define MAX_SIZE 1024
+#define MSG_STOP "exit"
+#define USER_JOINED "User joined: "
+#define USER_LEFT "User left: "
+
+mqd_t mq;
+char users[MAX_SIZE][MAX_SIZE];
+int user_count = 0;
+pthread_mutex_t user_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void notify_all_clients(const char *message) {
+    mqd_t mq_notify = mq_open(QUEUE_NAME, O_WRONLY);
+    if (mq_notify == -1) {
+        perror("Server: mq_open for notify");
+        exit(1);
+    }
+
+    if (mq_send(mq_notify, message, strlen(message) + 1, 0) == -1) {
+        perror("Server: mq_send for notify");
+        exit(1);
+    }
+
+    mq_close(mq_notify);
+}
+
+void *handle_client(void *arg) {
+    char buffer[MAX_SIZE];
+    mqd_t client_mq = *(mqd_t *)arg;
+
+    while (1) {
+        ssize_t bytes_read = mq_receive(client_mq, buffer, MAX_SIZE, NULL);
+        if (bytes_read < 0) {
+            perror("Server: mq_receive");
+            pthread_exit(NULL);
+        }
+        buffer[bytes_read] = '\0';
+
+        if (strncmp(buffer, MSG_STOP, strlen(MSG_STOP)) == 0) {
+            break;
+        }
+
+        notify_all_clients(buffer);
+    }
+
+    mq_close(client_mq);
+    pthread_exit(NULL);
+}
+
+int main() {
+    struct mq_attr attr;
+    attr.mq_flags = 0;
+    attr.mq_maxmsg = 10;
+    attr.mq_msgsize = MAX_SIZE;
+    attr.mq_curmsgs = 0;
+
+    mq = mq_open(QUEUE_NAME, O_CREAT | O_RDWR, 0644, &attr);
+    if (mq == -1) {
+        perror("Server: mq_open");
+        exit(1);
+    }
+
+    while (1) {
+        char buffer[MAX_SIZE];
+        ssize_t bytes_read = mq_receive(mq, buffer, MAX_SIZE, NULL);
+        if (bytes_read < 0) {
+            perror("Server: mq_receive");
+            exit(1);
+        }
+        buffer[bytes_read] = '\0';
+
+        if (strncmp(buffer, USER_JOINED, strlen(USER_JOINED)) == 0) {
+            pthread_mutex_lock(&user_mutex);
+            strncpy(users[user_count], buffer + strlen(USER_JOINED), MAX_SIZE - 1);
+            users[user_count][MAX_SIZE - 1] = '\0';
+            user_count++;
+            pthread_mutex_unlock(&user_mutex);
+            notify_all_clients(buffer);
+        } else if (strncmp(buffer, USER_LEFT, strlen(USER_LEFT)) == 0) {
+            pthread_mutex_lock(&user_mutex);
+            for (int i = 0; i < user_count; ++i) {
+                if (strcmp(users[i], buffer + strlen(USER_LEFT)) == 0) {
+                    for (int j = i; j < user_count - 1; ++j) {
+                        strncpy(users[j], users[j + 1], MAX_SIZE);
+                    }
+                    user_count--;
+                    break;
+                }
+            }
+            pthread_mutex_unlock(&user_mutex);
+            notify_all_clients(buffer);
+        } else {
+            notify_all_clients(buffer);
+        }
+    }
+
+    mq_close(mq);
+    mq_unlink(QUEUE_NAME);
+
+    return 0;
+}
