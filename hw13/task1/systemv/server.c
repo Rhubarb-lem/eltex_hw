087f9edf67fe64beaad3800472733c2670911ae3
Rhubarb-lem <revin2003@yandex.ru>
2024-06-15 16:31:13 +0700

    Clean

--- hw13/task1/systemv/server.c
+++ hw13/task1/systemv/server.c
@@ -19,40 +19,39 @@ int main() {
     attr.mq_msgsize = MAX_SIZE;
     attr.mq_curmsgs = 0;
 
-    // Открываем очередь сообщений для отправки клиенту
     server_mq = mq_open(SERVER_QUEUE_NAME, O_CREAT | O_WRONLY, 0644, &attr);
     if (server_mq == -1) {
         perror("error opening queue");
         exit(1);
     }
 
-    // Отправляем сообщение "Hi!" клиенту
+   
     strcpy(buffer, "Hi!");
     if (mq_send(server_mq, buffer, strlen(buffer) + 1, 0) == -1) {
         perror("error send message");
         exit(1);
     }
 
-    // Закрываем очередь для отправки
+   
     mq_close(server_mq);
 
-    // Открываем очередь для получения ответа от клиента
+ 
     client_mq = mq_open(CLIENT_QUEUE_NAME, O_CREAT | O_RDONLY, 0644, &attr);
     if (client_mq == -1) {
         perror("error opening queue");
         exit(1);
     }
 
-    // Ждем ответ от клиента
+
     if (mq_receive(client_mq, buffer, MAX_SIZE, NULL) == -1) {
         perror("error receive message");
         exit(1);
     }
 
-    // Выводим ответ клиента
+
     printf("Received message: %s\n", buffer);
 
-    // Закрываем и удаляем очереди
+
     mq_close(client_mq);
     mq_unlink(SERVER_QUEUE_NAME);
     mq_unlink(CLIENT_QUEUE_NAME);
