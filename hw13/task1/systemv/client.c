aae2aaa95c7a60c7976e9c5b80941edc9901676d
Rhubarb-lem <revin2003@yandex.ru>
2024-06-15 16:49:32 +0700

    Some fix

--- hw13/task1/systemv/client.c
+++ hw13/task1/systemv/client.c
@@ -17,14 +17,13 @@ int main() {
     int server_qid, client_qid;
     struct message msg;
 
-    // Создаем очередь сообщений сервера
+
     server_qid = msgget(SERVER_QUEUE_KEY, IPC_CREAT | 0666);
     if (server_qid == -1) {
         perror("error creating server queue");
         exit(1);
     }
 
-    // Отправляем сообщение на сервер
     msg.mtype = 1;
     strcpy(msg.mtext, "Hi!");
     if (msgsnd(server_qid, &msg, sizeof(msg.mtext), 0) == -1) {
@@ -32,14 +31,13 @@ int main() {
         exit(1);
     }
 
-    // Создаем очередь сообщений клиента
     client_qid = msgget(CLIENT_QUEUE_KEY, IPC_CREAT | 0666);
     if (client_qid == -1) {
         perror("error creating client queue");
         exit(1);
     }
 
-    // Принимаем ответное сообщение
+
     if (msgrcv(client_qid, &msg, sizeof(msg.mtext), 0, 0) == -1) {
         perror("error receiving message");
         exit(1);
@@ -47,7 +45,7 @@ int main() {
 
     printf("Received message: %s\n", msg.mtext);
 
-    // Удаляем очереди сообщений
+
     if (msgctl(server_qid, IPC_RMID, NULL) == -1) {
         perror("error removing server queue");
         exit(1);
