a7466cac0c371b6779b917c6c7b7950c31d384e1
Rhubarb-lem <revin2003@yandex.ru>
2024-06-23 00:34:01 +0700

    Create aflocal udp server client

---
+++ hw16/task1/udp_aflocal/client.c
@@ -0,0 +1,53 @@
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define BUFLEN 81
+#define SMSG "HI!"
+#define SOCKET_PATH "/tmp/udp_socket"
+
+int main()
+{
+    int sock;
+    struct sockaddr_un servAddr;
+    char buf[BUFLEN];
+
+    if ((sock = socket(AF_LOCAL, SOCK_STREAM, 0)) < 0)
+    {
+        perror("Error opening socket");
+        exit(1);
+    }
+
+    bzero((char *)&servAddr, sizeof(servAddr));
+    servAddr.sun_family = AF_LOCAL;
+    strncpy(servAddr.sun_path, SOCKET_PATH, sizeof(servAddr.sun_path) - 1);
+
+
+    if (connect(sock, (const struct sockaddr *)&servAddr, sizeof(servAddr)) < 0)
+    {
+        perror("Error connecting to server");
+        exit(1);
+    }
+
+
+    if (send(sock, SMSG, BUFLEN, 0) < 0)
+    {
+        perror("Error sending message");
+        exit(1);
+    }
+
+    if (recv(sock, buf, BUFLEN, 0) < 0)
+    {
+        perror("Error receiving message");
+        exit(1);
+    }
+
+    printf("Received message from server: %s\n", buf);
+
+    close(sock);
+
+    return 0;
+}
